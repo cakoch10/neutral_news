ab3dea5dbc2d01fa32db5f6b5ed94000f5048b22
cakoch10 <cakoch10@gmail.com>
2017-04-01 07:38:51 -0700

    added training data

--- computeURLs.py
+++ computeURLs.py
@@ -4,6 +4,64 @@ import newspaper
 #import urllib2
 import json
 
+
+def test1():
+    s = 0
+    for i in range(1, 10):
+        s = s+1
+        print(s)
+
+def scrapeFarRight():
+    farRight = newspaper.build('http://www.breitbart.com/big-government/',memoize_articles=False)
+    i = 0
+    for article in farRight.articles:
+        article.download()
+        article.parse()
+        with open("farRight/article" + str(i) + ".txt", 'a') as out:
+            out.write(article.text + '\n')
+        i = i+1
+
+def scrapeCenterRight():
+    centerR = newspaper.build('http://foxnews.com/us.html',memoize_articles=False)
+    i = 0
+    for article in centerR.articles:
+        article.download()
+        article.parse()
+        with open("centerRight/article" + i + ".txt", 'a') as out:
+            out.write(article.text + '\n')
+        i = i+1
+
+def scrapeModerate():
+    moderate = newspaper.build('http://cbsnews.com/us/',memoize_articles=False)
+    i = 0
+    for article in moderate.articles:
+        article.download()
+        article.parse()
+        with open("moderate/article" + i + ".txt", 'a') as out:
+            out.write(article.text + '\n')
+        i = i+1
+
+def scrapeCenterLeft():
+    centerL = newspaper.build('http://bbc.com/news/world/us_and_canada')
+    i = 0
+    for article in centerL.articles:
+        article.download()
+        article.parse()
+        with open("centerLeft/article" + i + ".txt", 'a') as out:
+            out.write(article.text + '\n')
+        i = i+1
+
+
+def scrapeFarLeft():
+    farLeft = newspaper.build('http://dailykos.com/blogs/main')
+    i = 0
+    for article in farLeft.articles:
+        article.download()
+        article.parse()
+        with open("centerLeft/article" + i + ".txt", 'a') as out:
+            out.write(article.text + '\n')
+        i = i+1
+
 def scrapeNews():
     # urllib2.urlopen("http://example.com/foo/bar").read()
     # https://newsapi.org/v1/articles?source=breitbart-news&sortBy=&apiKey=989c61d67d234d3fa19d1ec4e6c5b361
@@ -19,18 +77,8 @@ def scrapeNews():
     # print json.dumps(L2, indent = 4, sort_keys = True)
     #print len(L2['articles'])
     # http://www.breitbart.com/big-government/ cbs_paper = newspaper.build('http://cbs.com')
-
-    farRight = newspaper.build('http://www.breitbart.com/big-government/',memoize_articles=False)
     # strFR = ""
     #farRight = newspaper.build('http://breitbart.com/big-government')
-
-    for article in farRight.articles:
-        print(article.url)
-        #strFR += article.url + '\n'
-        with open("farRight.txt", 'a') as out:
-            out.write(article.url + '\n')
-        #print(article.url)
-    print("test")
     #print(strFR)
     #print(len(farRight.articles))
     #farRight = Article(url="http://www.breitbart.com/big-government/", language='en')
