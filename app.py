fdf5d7094ad83011b9779080c7f6b5b171b296ba
Wes T4N <wesleyt1998@gmail.com>
2017-04-01 15:42:02 -0400

    first html + py debu

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, render_template, request, json
+from news import *
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +10,9 @@ def index_view():
 @app.route('/checkURL',methods=['POST'])
 def checkURL():
     _url = request.form['inputURL']
+    google_scrape(_url);
+    url_arr = _url.pickArticlesWithOutLabels();
+    print(url_arr);
     return json.dumps({'url': _url})
     # pass url to method and get json of 5 urls: far left, left, moderate, right, and far right
     # something like return json.dumps({'html':'<span>All fields good !!</span>'})
