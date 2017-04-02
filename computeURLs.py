46f1ab3aeed3215e4e185eb2f367eceb87187bfa
Caleb Koch <cakoch10@gmail.com>
2017-04-02 02:30:29 -0400

    modified main python

--- computeURLs.py
+++ computeURLs.py
@@ -1,15 +1,31 @@
 #import urllib.request
 from __future__ import unicode_literals
 import newspaper
-import word2vec
+#import word2vec
+#import rpy2.robjects as ro
 
 #from newspaper import Article
 #import urllib2
 import json
 import nltk
 import os
-from nltk.corpus.reader.plaintext import PlaintextCorpusReader
+#from nltk.corpus.reader.plaintext import PlaintextCorpusReader
+import csv
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+def compareSimilarity(urlone, urltwo):
+    first_article = Article(url=urlone, language='en')
+    first_article.download()
+    first_article.parse()
+    second_article = Article(url=urltwo, language='en')
+    second_article.download()
+    second_article.parse()
+    doc[0] = first_article.text
+    doc[1] = second_article.text
+    documents = doc
+    tfidf = TfidfVectorizer().fit_transform(documents)
+    pairwise_similarity = tfidf * tfidf.T
+    return pairwise_similarity
 
 def buildModel():
     corpusdir = 'farRight/' # Directory of corpus.
@@ -32,6 +48,21 @@ def buildModel():
     input_file.close()
     #word2vec.doc2vec('alldata.txt', cbow=0, size=100, window=10, negative=5, hs=0, sample='1e-4', threads=12, iter_=20, min_count=1, verbose=True)
     word2vec.doc2vec('alldata.txt', 'vectors.bin', cbow=0, size=100, window=10, negative=5, hs=0, sample='1e-4', threads=12, iter_=20, min_count=1, verbose=True)
+
+    f = open('final_table.csv', 'rb')
+    # reader = csv.reader(f)
+    # headers = reader.next()
+    # column = {}
+    # for h in headers:
+    #     column[h] = []
+    # for row in reader:
+    #     for h, v in zip(headers, row):
+    #         column[h].append(v)
+    # #column contains a dictionary of the data we need to load to model
+    # arr[0] = column[""]
+    # X =
+
+    ##########EXECUTE CODE FROM WITHIN PYTHON!!!
 
 
     #newcorpus = PlaintextCorpusReader(corpusdir, '.*')
